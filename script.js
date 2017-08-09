263d170d954d1df3d02dca3526dfebccb0d4b363
Lucas <viper20@poczta.fm>
2017-08-09 17:47:25 +0200

    fixed little devil

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $("span:even").css('color', 'red');
 var paragraphs = $("p");
     paragraphs.each(function(index, element) {
     
-    var button = '<button class="btn" data-imp="' + index + '">Click me</button>'
+    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
     $(element).append(button)
 });
 
